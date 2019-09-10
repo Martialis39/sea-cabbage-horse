7f4ac3fa38fa6ce519f3e447b862858edc89f46d
Sass <sass@velvet.ee>
2019-09-10 11:02:23 +0300

    Refactor app to use useEffect, add DndProvider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
-import { useAllPersons } from "./hooks/persons";
+import React, { useState, useCallback, useEffect } from "react";
+
 import Header from "./components/Header";
 import Modal from "./components/Modal";
-import IconOrganization from "./assets/IconOrganization";
 import PersonItem from "./components/PersonItem";
-import Test from "./components/Test";
 import { PersonsList, Title } from "./App.style";
+// DND
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+// Data
+import PersonsService from "./services/persons";
 
 function App() {
-  let persons = useAllPersons();
+  const [persons, setPersons] = useState(null);
+  useEffect(() => {
+    if (typeof window !== undefined && persons == null) {
+      const store = window.localStorage;
+      const personsFromLocalStorage = store.getItem("test_assignment_people");
+      if (!personsFromLocalStorage) {
+        PersonsService.getAllPersons().then(response => {
+          store.setItem(
+            "test_assignment_people",
+            JSON.stringify(response.data)
+          );
+          setPersons(response.data);
+        });
+      } else {
+        setPersons(JSON.parse(personsFromLocalStorage));
+      }
+    }
+  }, [persons]);
   const [modalRender, setModalRender] = useState(null);
   console.log("modalRender is ", modalRender);
 
-  return <Test />;
+  const moveCard = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragCard = persons[dragIndex];
+      const newPersons = persons.slice();
+      newPersons.splice(dragIndex, 1);
+      newPersons.splice(hoverIndex, 0, dragCard);
+      setPersons(newPersons);
+    },
+    [persons]
+  );
+
   return (
     <main className="App">
       <Header />
@@ -22,19 +52,23 @@ function App() {
       <Title>
         <h2>People's list</h2>
       </Title>
-      {persons && (
-        <PersonsList>
-          {persons.map((person, index) => {
-            return (
-              <PersonItem
-                key={index}
-                {...person}
-                setModalRender={setModalRender}
-              />
-            );
-          })}
-        </PersonsList>
-      )}
+      <DndProvider backend={HTML5Backend}>
+        {persons && (
+          <PersonsList>
+            {persons.map((person, index) => {
+              return (
+                <PersonItem
+                  moveCard={moveCard}
+                  key={index}
+                  index={index}
+                  {...person}
+                  setModalRender={setModalRender}
+                />
+              );
+            })}
+          </PersonsList>
+        )}
+      </DndProvider>
     </main>
   );
 }
