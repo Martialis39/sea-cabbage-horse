7ca1b0226c3fce1d105713469f6fb9c50e1c3ca9
Sass <sass@velvet.ee>
2019-09-10 12:16:08 +0300

    Remove shadow from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
+import React from "react";
+import logo from "../assets/logo.svg";
 
-function Header(){
-  return <header style={{backgroundColor: '#000', boxShadow: '0px 4px 5px 0px rgba(0,0,0,0.3)'}}>
-      <img style={{width: '173px'}} src={logo} alt="Pipedrive" />
-  </header>
+function Header() {
+  return (
+    <header style={{ backgroundColor: "#000" }}>
+      <img style={{ width: "173px" }} src={logo} alt="Pipedrive" />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
