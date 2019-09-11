bdfe69ed48bcc70ea4f5aad312f5ffea6fa2b3fe
Sass <sass@velvet.ee>
2019-09-11 11:12:00 +0300

    Refactor header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { StyledHeader } from "./Header.style";
 
 function Header() {
   return (
-    <header style={{ backgroundColor: "#000" }}>
+    <StyledHeader>
       <img style={{ width: "173px" }} src={logo} alt="Pipedrive" />
-    </header>
+    </StyledHeader>
   );
 }
 
