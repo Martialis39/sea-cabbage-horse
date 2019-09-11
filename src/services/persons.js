b01b2e901829ee5c52dd3cfb55835005f7521823
Sass <sass@velvet.ee>
2019-09-11 11:13:14 +0300

    add a delete request

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const getAllPersons = () => {
-  const request = axios.get(process.env.REACT_APP_GET_PERSONS);
+  const request = axios.get(process.env.REACT_APP_API_GET_PERSONS);
   return request.then(response => response.data);
 };
 
-export default { getAllPersons };
+const deletePersonById = id => {
+  const request = axios.delete(
+    `${process.env.REACT_APP_API_PERSONS}/${id}/${process.env.REACT_APP_API_TOKEN}`
+  );
+  return request.then(response => response);
+};
+export default { getAllPersons, deletePersonById };
