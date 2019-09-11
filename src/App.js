ab265fa7642af5aba5c2468b632ff6412d02d0e0
Sass <sass@velvet.ee>
2019-09-11 14:22:45 +0300

    Add custom order, refactor styles

--- src/App.js
+++ src/App.js
@@ -2,31 +2,85 @@ import React, { useState, useCallback, useEffect } from "react";
 
 import Header from "./components/Header";
 import Modal from "./components/Modal";
+import Pagination from "./components/Pagination";
 import PersonItem from "./components/PersonItem";
-import { StyledPersonsList, Title } from "./App.style";
+import Notification from "./components/Notification";
+import { StyledPersonsList, StyledTitle, StyledButton } from "./App.style";
 // DND
 import { DndProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 // Data
 import PersonsService from "./services/persons";
-import Pagination from "./components/Pagination";
+import { CustomFields } from "./constants/index";
 
 function App() {
   const [persons, setPersons] = useState(null);
+  const [initialOrder, setInitialOrder] = useState(null);
+  const [customOrder, setCustomOrder] = useState(null);
+  const [notification, setNotification] = useState(null);
+
+  const compareOrders = () => {
+    if (initialOrder && customOrder) {
+      const elementsOutOfPlace = initialOrder.reduce(
+        (result, currentElement, index) => {
+          // Map over customOrder for easy index checking
+          const customOrderAsIDs = customOrder.map(e => e.id);
+          // Check index is not outside of range
+          if (index < customOrderAsIDs.length) {
+            // Compare the IDs
+
+            if (currentElement.id !== customOrder[index].id) {
+              const elementOutOfPlace = {
+                id: currentElement.id,
+                // Map over custom order array and check the new index of the out of place element
+                newIndex: customOrderAsIDs.indexOf(currentElement.id)
+              };
+              return result.concat(elementOutOfPlace);
+            }
+          }
+          return result;
+        },
+        []
+      );
+      debugger;
+      if (elementsOutOfPlace) {
+        PersonsService.updatePersonOrder(elementsOutOfPlace);
+        setNotification("Order updated");
+      }
+    }
+  };
 
   useEffect(() => {
     if (persons === null) {
-      PersonsService.getAllPersons().then(response => {
-        setPersons(response.data);
-      });
+      PersonsService.getAllPersons()
+        .then(response => {
+          const peopleFromAPI = response.data;
+          setInitialOrder(
+            peopleFromAPI.map(e => {
+              return {
+                order: e[CustomFields.order],
+                id: e.id
+              };
+            })
+          );
+          setPersons(peopleFromAPI);
+        })
+        .catch(err => {
+          setNotification("Unable to fetch persons");
+        });
     }
   }, [persons]);
   const [modalRender, setModalRender] = useState(null);
 
   const deletePerson = id => {
-    debugger;
     const newPersons = persons.filter(e => e.id !== id);
-    PersonsService.deletePersonById(id).then(console.log);
+    PersonsService.deletePersonById(id)
+      .then(res => {
+        setNotification("Person deleted");
+      })
+      .catch(err => {
+        setNotification("Something went wrong");
+      });
     setPersons(newPersons);
   };
   const moveCard = useCallback(
@@ -36,20 +90,43 @@ function App() {
       newPersons.splice(dragIndex, 1);
       newPersons.splice(hoverIndex, 0, dragCard);
       setPersons(newPersons);
+      setCustomOrder(
+        newPersons.map(e => {
+          return {
+            order: e[CustomFields.order],
+            id: e.id
+          };
+        })
+      );
     },
     [persons]
   );
 
   return (
     <main className="App">
+      {notification && (
+        <Notification
+          setNotification={setNotification}
+          message={notification}
+        />
+      )}
       {modalRender && (
         <Modal render={modalRender} setModalRender={setModalRender} />
       )}
       <Header />
-      <Title>
+      <StyledTitle>
         <h2>People's list</h2>
-      </Title>
+      </StyledTitle>
       <DndProvider backend={HTML5Backend}>
+        <StyledButton
+          disabled={!customOrder}
+          onClick={() => {
+            compareOrders();
+          }}
+        >
+          Save custom order
+        </StyledButton>
+
         {persons && (
           <StyledPersonsList>
             <Pagination>
--- src/App.style.js
+++ src/App.style.js
@@ -8,8 +8,21 @@ export const StyledPersonsList = styled.div`
   }
 `;
 
-export const Title = styled.div`
+export const StyledTitle = styled.div`
   padding: 20px 20px;
   box-shadow: 0px 1px 3px #00000030;
   font-weight: 700;
+`;
+
+export const StyledButton = styled.button`
+  background: white;
+  border: none;
+  border-radius: 2px;
+  font-weight: 700;
+  font-family: "Open Sans", sans-serif;
+  margin-top: 20px;
+  margin-left: 20px;
+  padding: 10px 25px;
+  border: 1px solid #26292c70;
+  cursor: pointer;
 `;
